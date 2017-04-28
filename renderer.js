f9e64f482cb047f56aa45fc16d111326611906c1
Rowland Rose <rowlandrose@gmail.com>
2017-04-27 22:09:30 -0400

    First communication with web midi

--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+navigator.requestMIDIAccess().then(function() {
+    // Success
+    document.querySelector("#output").innerHTML = 'success!';
+}, function() {
+    // Fail
+    document.querySelector("#output").innerHTML = 'fail...';
+});
